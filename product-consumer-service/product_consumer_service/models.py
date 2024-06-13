148cb9ff411263a772a92a55b8c3c3f6d37f4dd4
Ammar Aamir <ammaraamir609@gmail.com>
2024-06-13 10:47:46 +0500

    minor changes

--- product-consumer-service/product_consumer_service/models.py
+++ product-consumer-service/product_consumer_service/models.py
@@ -4,8 +4,8 @@ from sqlmodel import SQLModel
 
 
 class Product (SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    name: str = Field()
+    id: int = Field(default=None)
+    name: str = Field(primary_key=True)
     description: str = Field()
     price: float = Field()
     quantity: int = Field()
