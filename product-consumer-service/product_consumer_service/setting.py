f947a124295eb7c7ec699a2e16ea837dcb5da0a4
Ammar Aamir <ammaraamir609@gmail.com>
2024-06-13 12:36:07 +0500

    minor changes

--- product-consumer-service/product_consumer_service/setting.py
+++ product-consumer-service/product_consumer_service/setting.py
@@ -12,5 +12,5 @@ BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
 KAFKA_PRODUCT_TOPIC = config("KAFKA_PRODUCT_TOPIC", cast=str)
 KAFKA_CONSUMER_GROUP_ID = config("KAFKA_CONSUMER_GROUP_ID", cast=str)
 
-DATABASE_URL = config("DATABASE_URL", cast=Secret)
+DATABASE_URL = config("DATABASE_URL", cast=str)
 TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
