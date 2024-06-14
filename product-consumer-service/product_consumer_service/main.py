f54c6e71d4048857f584902849be07f3c0db2e8e
Ammar Aamir <ammaraamir609@gmail.com>
2024-06-14 17:17:26 +0500

    minor changes

--- product-consumer-service/product_consumer_service/main.py
+++ product-consumer-service/product_consumer_service/main.py
@@ -17,7 +17,7 @@ logging.basicConfig(level= logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class Product (SQLModel, table=True):
+class Pproduct (SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str
     description: str
@@ -32,88 +32,88 @@ async def lifespan(app: FastAPI):
     create_tables()
     logger.info("Tables Created")
 
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(consume_products())
+    # loop = asyncio.get_event_loop()
+    # task = loop.create_task(consume_products())
     yield
 
-    task.cancel()
-    await task
+    # task.cancel()
+    # await task
 
 
 MAX_RETRIES = 5
 RETRY_INTERVAL = 10
 
-async def consume_products():
-    retries = 0
+# async def consume_products():
+#     retries = 0
 
-    while retries < MAX_RETRIES:
-        try:
-            consumer = AIOKafkaConsumer(
-                KAFKA_PRODUCT_TOPIC,
-                bootstrap_servers=BOOTSTRAP_SERVER,
-                group_id=KAFKA_CONSUMER_GROUP_ID
-            )
+#     while retries < MAX_RETRIES:
+#         try:
+#             consumer = AIOKafkaConsumer(
+#                 KAFKA_PRODUCT_TOPIC,
+#                 bootstrap_servers=BOOTSTRAP_SERVER,
+#                 group_id=KAFKA_CONSUMER_GROUP_ID
+#             )
 
-            await consumer.start()
-            logger.info("CONSUMER STARTED LETSS GOOOOOOOO....")
-            try:
-                async for msg in consumer:
-                    try:
-                        product = product_pb2.Product()
-                        product.ParseFromString(msg.value)
-                        logger.info(f"Received Message: {product}")
+#             await consumer.start()
+#             logger.info("CONSUMER STARTED LETSS GOOOOOOOO....")
+#             try:
+#                 async for msg in consumer:
+#                     try:
+#                         product = product_pb2.Product()
+#                         product.ParseFromString(msg.value)
+#                         logger.info(f"Received Message: {product}")
 
-                        with Session(engine) as session:
-                            if product.operation == product_pb2.OperationType.CREATE:
-                                new_product = Product(
-                                    id=product.id,
-                                    name=product.name,
-                                    description=product.description,
-                                    price=product.price,
-                                    quantity=product.quantity
-                                )
-                                session.add(new_product)
-                                session.commit()
-                                session.refresh(new_product)
-                                logger.info(f'Product added to db: {new_product}')
+#                         with Session(engine) as session:
+#                             if product.operation == product_pb2.OperationType.CREATE:
+#                                 new_product = Product(
+#                                     id=product.id,
+#                                     name=product.name,
+#                                     description=product.description,
+#                                     price=product.price,
+#                                     quantity=product.quantity
+#                                 )
+#                                 session.add(new_product)
+#                                 session.commit()
+#                                 session.refresh(new_product)
+#                                 logger.info(f'Product added to db: {new_product}')
                             
-                            elif product.operation == product_pb2.OperationType.UPDATE:
-                                existing_product = session.exec(select(Product).where(Product.id == product.id)).first()
-                                if existing_product:
-                                    existing_product.name = product.name
-                                    existing_product.description = product.description
-                                    existing_product.price = product.price
-                                    existing_product.quantity = product.quantity
-                                    session.add(existing_product)
-                                    session.commit()
-                                    session.refresh(existing_product)
-                                    logger.info(f'Product updated in db: {existing_product}')
-                                else:
-                                    logger.warning(f"Product with ID {product.id} not found")
+#                             elif product.operation == product_pb2.OperationType.UPDATE:
+#                                 existing_product = session.exec(select(Product).where(Product.id == product.id)).first()
+#                                 if existing_product:
+#                                     existing_product.name = product.name
+#                                     existing_product.description = product.description
+#                                     existing_product.price = product.price
+#                                     existing_product.quantity = product.quantity
+#                                     session.add(existing_product)
+#                                     session.commit()
+#                                     session.refresh(existing_product)
+#                                     logger.info(f'Product updated in db: {existing_product}')
+#                                 else:
+#                                     logger.warning(f"Product with ID {product.id} not found")
 
-                            elif product.operation == product_pb2.OperationType.DELETE:
-                                existing_product = session.exec(select(Product).where(Product.id == product.id)).first()
-                                if existing_product:
-                                    session.delete(existing_product)
-                                    session.commit()
-                                    logger.info(f"Product with ID {product.id} successfully deleted")
-                                else:
-                                    logger.warning(f"Product with ID {product.id} not found for deletion")
+#                             elif product.operation == product_pb2.OperationType.DELETE:
+#                                 existing_product = session.exec(select(Product).where(Product.id == product.id)).first()
+#                                 if existing_product:
+#                                     session.delete(existing_product)
+#                                     session.commit()
+#                                     logger.info(f"Product with ID {product.id} successfully deleted")
+#                                 else:
+#                                     logger.warning(f"Product with ID {product.id} not found for deletion")
 
-                    except Exception as e:
-                        logger.error(f"Error processing message: {e}")
+#                     except Exception as e:
+#                         logger.error(f"Error processing message: {e}")
 
-            finally:
-                await consumer.stop()
-                logger.info("Consumer stopped")
-            return
+#             finally:
+#                 await consumer.stop()
+#                 logger.info("Consumer stopped")
+#             return
         
-        except KafkaConnectionError:
-            retries += 1 
-            print(f"Kafka connection failed. Retrying {retries}/{MAX_RETRIES}...")
-            await asyncio.sleep(RETRY_INTERVAL)
+#         except KafkaConnectionError:
+#             retries += 1 
+#             print(f"Kafka connection failed. Retrying {retries}/{MAX_RETRIES}...")
+#             await asyncio.sleep(RETRY_INTERVAL)
         
-    raise Exception("Failed to connect to Kafka broker after several retries")
+#     raise Exception("Failed to connect to Kafka broker after several retries")
 
 
 app = FastAPI(lifespan=lifespan, title="Product Consumer Service", version='1.0.0')
