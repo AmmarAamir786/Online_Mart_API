2120fd5481cb40f7647b70fab2389dcc804b063a
Ammar Aamir <ammaraamir609@gmail.com>
2024-07-15 21:14:24 +0500

    fixing order service

--- order-consumer-service/order_consumer_service/main.py
+++ order-consumer-service/order_consumer_service/main.py
@@ -1,10 +1,10 @@
 import asyncio
 from contextlib import asynccontextmanager
 from typing import List
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Session, select
 
-from order_consumer_service.models import Order
+from order_consumer_service.models import Order, OrderProduct
 from order_consumer_service.db import create_tables, engine, get_session
 from order_consumer_service.setting import KAFKA_INVENTORY_UPDATE_TOPIC
 
@@ -39,16 +39,37 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, title="Order Consumer Service", version='1.0.0')
 
-@app.get("/orders/", response_model=List[Order])
-async def get_orders():
-    with Session(engine) as session:
-        orders = session.exec(select(Order)).all()
-        return orders
+@app.get("/orders/")
+async def get_orders(session = Depends(get_session)):
+    orders_query = await session.exec(select(Order))
+    orders = orders_query.all()
 
-@app.get("/orders/{order_id}", response_model=Order)
-async def get_order(order_id: str):
-    with Session(engine) as session:
-        order = session.exec(select(Order).where(Order.order_id == order_id)).first()
-        if not order:
-            raise HTTPException(status_code=404, detail="Order not found")
-        return order
+    result = []
+    for order in orders:
+        products_query = await session.exec(select(OrderProduct).where(OrderProduct.order_id == order.order_id))
+        products = products_query.all()
+
+        order_dict = {
+            "order_id": order.order_id,
+            "products": [{"product_id": product.product_id, "quantity": product.quantity} for product in products]
+        }
+        result.append(order_dict)
+
+    return result
+
+@app.get("/orders/{order_id}")
+async def get_order(order_id: str, session = Depends(get_session)):
+    query = await session.exec(select(Order).where(Order.order_id == order_id))
+    order = query.first()
+    if not order:
+        return {"error": "Order not found"}
+
+    products_query = await session.exec(select(OrderProduct).where(OrderProduct.order_id == order_id))
+    products = products_query.all()
+
+    order_dict = {
+        "order_id": order.order_id,
+        "products": [{"product_id": product.product_id, "quantity": product.quantity} for product in products]
+    }
+
+    return order_dict
