88cfcfa42dd07af40ffc8b0ea389eb1b1a8bb19c
Ammar Aamir <ammaraamir609@gmail.com>
2024-07-15 11:25:17 +0500

    fixing order service

--- product-service/product_service/main.py
+++ product-service/product_service/main.py
@@ -1,57 +1,16 @@
-import asyncio
 from contextlib import asynccontextmanager
-import logging
 from typing import Annotated, AsyncGenerator
 from fastapi import Depends, FastAPI
 
 from product_service.proto import product_pb2, operation_pb2
 
 from product_service.models import Product, ProductUpdate
-from product_service.setting import BOOTSTRAP_SERVER, KAFKA_PRODUCT_TOPIC
+from product_service.setting import KAFKA_PRODUCT_TOPIC
 from aiokafka import AIOKafkaProducer
-from aiokafka.errors import KafkaConnectionError
-from aiokafka.admin import AIOKafkaAdminClient, NewTopic
+from product_service.utils.producer import kafka_producer
+from product_service.utils.topic import create_topic
+from product_service.utils.logger import logger
 
-
-MAX_RETRIES = 5
-RETRY_INTERVAL = 10
-
-
-async def create_topic(topic:str):
-    admin_client = AIOKafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVER)
-
-    retries = 0
-
-    while retries < MAX_RETRIES:
-        try:
-            await admin_client.start()
-            topic_list = [NewTopic(name=topic,
-                                num_partitions=2, 
-                                replication_factor=1)]
-            try:
-                await admin_client.create_topics(new_topics=topic_list, validate_only=False)
-                logger.info(f"Topic '{topic}' created successfully")
-            except Exception as e:
-                logger.info(f"Failed to create topic '{topic}': {e}")
-            finally:
-                await admin_client.close()
-            return
-        
-        except KafkaConnectionError:
-            retries += 1 
-            logger.info(f"Kafka connection failed. Retrying {retries}/{MAX_RETRIES}...")
-            await asyncio.sleep(RETRY_INTERVAL)
-        
-    raise Exception("Failed to connect to kafka broker after several retries")
-
-
-async def kafka_producer():
-    producer = AIOKafkaProducer(bootstrap_servers=BOOTSTRAP_SERVER)
-    await producer.start()
-    try:
-        yield producer
-    finally:
-        await producer.stop()
 
 
 @asynccontextmanager
@@ -61,15 +20,6 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 
 app = FastAPI(lifespan=lifespan, title="Product Service", version='1.0.0')
-
-
-# @app.get('/')
-# async def root() -> Any:
-#     return {"message": "Welcome to products section"}
-
-
-logging.basicConfig(level= logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 @app.post('/products/')
