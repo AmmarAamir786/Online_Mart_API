593b305c6bf4f5642e893ca50d0ca756d38a1b5c
Ammar Aamir <ammaraamir609@gmail.com>
2024-07-18 12:34:49 +0500

    fixing order service

--- product-service/product_service/utils/producer.py
+++ product-service/product_service/utils/producer.py
@@ -1,11 +1,11 @@
 from aiokafka import AIOKafkaProducer
-from aiokafka.helpers import ConfluentSchemaRegistry
+
 from product_service.setting import BOOTSTRAP_SERVER, KAFKA_SCHEMA_REGISTRY_URL
 
 
 async def kafka_producer():
-    producer = AIOKafkaProducer(bootstrap_servers=BOOTSTRAP_SERVER,
-                                schema_registry=ConfluentSchemaRegistry(url=KAFKA_SCHEMA_REGISTRY_URL))
+    producer = AIOKafkaProducer(bootstrap_servers=BOOTSTRAP_SERVER)
+
     await producer.start()
     try:
         yield producer
