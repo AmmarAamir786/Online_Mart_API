59f88a0007dd441b3b15eaa5eaa03b161017d0f7
Ammar Aamir <ammaraamir609@gmail.com>
2024-07-18 13:11:59 +0500

    trying to implement schema registry

--- product-service/product_service/main.py
+++ product-service/product_service/main.py
@@ -1,12 +1,13 @@
 from contextlib import asynccontextmanager
 from typing import Annotated, AsyncGenerator
+from aiokafka import AIOKafkaProducer
 from fastapi import Depends, FastAPI
 
 from product_service.proto import product_pb2, operation_pb2
 from product_service.models import Product, ProductUpdate
 from product_service.setting import KAFKA_PRODUCT_TOPIC
 
-from product_service.utils.producer import create_kafka_producer
+from product_service.utils.producer import kafka_producer
 from product_service.utils.topic import create_topic
 from product_service.utils.logger import logger
 
@@ -23,63 +24,52 @@ app = FastAPI(lifespan=lifespan, title="Product Service", version='1.0.0')
 @app.post('/products/')
 async def create_product(
     product: Product,
-    producer_dep: Annotated[tuple, Depends(create_kafka_producer)]):
+    producer: Annotated[AIOKafkaProducer, Depends(kafka_producer)]
+):
     
-    producer, protobuf_serializer = producer_dep
-    product_proto = product_pb2.Product(
-        name=product.name,
-        product_id=product.product_id,
-        price=product.price,
-        category=product.category,
-        description=product.description,
-        operation=operation_pb2.OperationType.CREATE
-    )
+    product_proto = product_pb2.Product()
+    product_proto.name = product.name
+    product_proto.product_id = product.product_id
+    product_proto.price = product.price
+    product_proto.category = product.category
+    product_proto.description = product.description
+    product_proto.operation = operation_pb2.OperationType.CREATE
 
     logger.info(f"Received Message: {product_proto}")
-    serialized_product = protobuf_serializer(product_proto, None)
+
+    serialized_product = product_proto.SerializeToString()
     await producer.send_and_wait(KAFKA_PRODUCT_TOPIC, serialized_product)
 
-    return {"Product": "Created"}
+    return {"Product" : "Created"}
 
 
 @app.put('/products')
-async def edit_product(
-    product: ProductUpdate, 
-    id: int, 
-    producer_dep: Annotated[tuple, Depends(create_kafka_producer)]):
+async def edit_product(product: ProductUpdate, id:int, producer: Annotated[AIOKafkaProducer, Depends(kafka_producer)]):
 
-    producer, protobuf_serializer = producer_dep
     logger.info(f"Received product data for update: {product}")
 
-    product_proto = product_pb2.Product(
-        id=id,
-        product_id=product.product_id,
-        name=product.name,
-        price=product.price,
-        category=product.category,
-        description=product.description,
-        operation=operation_pb2.OperationType.UPDATE
-    )
-    
-    serialized_product = protobuf_serializer(product_proto, None)
+    product_proto = product_pb2.Product()
+    product_proto.id = id
+    product_proto.product_id = product.product_id
+    product_proto.name = product.name
+    product_proto.price = product.price
+    product_proto.category = product.category
+    product_proto.description = product.description
+    product_proto.operation = operation_pb2.OperationType.UPDATE
+        
+    serialized_product = product_proto.SerializeToString()
     await producer.send_and_wait(KAFKA_PRODUCT_TOPIC, serialized_product)
 
     return {"Product": "Updated"}
-
+    
 
 @app.delete('/products/')
-async def delete_product(
-    id: int, 
-    producer_dep: Annotated[tuple, Depends(create_kafka_producer)]):
+async def delete_product(id: int, producer: Annotated[AIOKafkaProducer, Depends(kafka_producer)]):
+    product_proto = product_pb2.Product()
+    product_proto.id = id
+    product_proto.operation = operation_pb2.OperationType.DELETE
 
-    producer, protobuf_serializer = producer_dep
-
-    product_proto = product_pb2.Product(
-        id=id, 
-        operation=operation_pb2.OperationType.DELETE
-    )
-
-    serialized_product = protobuf_serializer(product_proto, None)
+    serialized_product = product_proto.SerializeToString()
     await producer.send_and_wait(KAFKA_PRODUCT_TOPIC, serialized_product)
 
-    return {"Product": "Deleted"}
+    return {"Product" : "Deleted"}
--- product-service/product_service/utils/producer.py
+++ product-service/product_service/utils/producer.py
@@ -1,19 +1,13 @@
 from aiokafka import AIOKafkaProducer
-from confluent_kafka.schema_registry import SchemaRegistryClient, Schema
-from confluent_kafka.serialization import ProtobufSerializer
 
 from product_service.setting import BOOTSTRAP_SERVER, KAFKA_SCHEMA_REGISTRY_URL
 
 
-async def create_kafka_producer():
-    schema_registry_client = SchemaRegistryClient({'url': KAFKA_SCHEMA_REGISTRY_URL})
-    schema = schema_registry_client.get_latest_version('product-schema')
-    protobuf_serializer = ProtobufSerializer(schema_registry_client)
-
+async def kafka_producer():
     producer = AIOKafkaProducer(bootstrap_servers=BOOTSTRAP_SERVER)
-    await producer.start()
 
+    await producer.start()
     try:
-        yield producer, protobuf_serializer
+        yield producer
     finally:
         await producer.stop()
