ec0684e25e38ecd6d2373aac762d71978cdd3da6
Ammar Aamir <ammaraamir609@gmail.com>
2024-07-19 21:46:51 +0500

    kong

--- gateway-startup.sh
+++ gateway-startup.sh
@@ -1,35 +1,48 @@
-#!/bin/sh
-
-# Install curl
-apk update
-apk add --no-cache curl
+#!/bin/bash
 
 # Wait until Kong is fully up
-until $(curl --output /dev/null --silent --head --fail http://kong:8001); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:8001); do
     echo "Waiting for Kong to be up..."
     sleep 5
 done
 
 # Add services and routes to Kong
-declare -A services_and_routes
-services_and_routes=(
-    ["product-service:8011"]="products"
-    ["product-consumer-service:8012"]="products"
-    ["order-service:8013"]="orders"
-    ["order-consumer-service:8014"]="orders"
-    ["inventory-service:8015"]="inventory"
-    ["inventory-consumer-service:8016"]="inventory"
-)
+services=("product-service:8011" "product-consumer-service:8012" "order-service:8013" "order-consumer-service:8014" "inventory-service:8015" "inventory-consumer-service:8016")
+routes=("products" "products" "orders" "orders" "inventory" "inventory")
 
-for service in "${!services_and_routes[@]}"; do
-    route="${services_and_routes[$service]}"
+for i in "${!services[@]}"; do
+    service="${services[$i]}"
+    route="${routes[$i]}"
+    method=""
+
+    if [[ "$route" == "products" ]]; then
+        if [[ "$service" == "product-service:8011" ]]; then
+            method="POST,PUT,DELETE"
+        else
+            method="GET"
+        fi
+    elif [[ "$route" == "orders" ]]; then
+        if [[ "$service" == "order-service:8013" ]]; then
+            method="POST,PUT,DELETE"
+        else
+            method="GET"
+        fi
+    elif [[ "$route" == "inventory" ]]; then
+        if [[ "$service" == "inventory-service:8015" ]]; then
+            method="POST,PUT,DELETE"
+        else
+            method="GET"
+        fi
+    fi
+
     # Add service
-    curl -i -X POST http://kong:8001/services/ \
-        --data "name=${service}" \
+    curl -i -X POST http://localhost:8001/services/ \
+        --data "name=${route}-${service}" \
         --data "url=http://${service}"
 
     # Add route
-    curl -i -X POST http://kong:8001/services/${service}/routes \
+    curl -i -X POST http://localhost:8001/services/${route}-${service}/routes \
         --data "paths[]=/api/${route}" \
+        --data "methods[]=${method}" \
         --data "strip_path=false"
 done
