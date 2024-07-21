ca575832a4efd2d95589fb74ed7b85a6f200d086
Ammar Aamir <ammaraamir609@gmail.com>
2024-07-21 12:06:04 +0500

    kong

--- kong/gateway-startup.sh
+++ kong/gateway-startup.sh
@@ -5,31 +5,34 @@ apk update
 apk add --no-cache curl
 
 # Wait until Kong is fully up
-until $(curl --output /dev/null --silent --head --fail http://kong:8001); do
+until curl --output /dev/null --silent --head --fail http://kong:8001; do
     echo "Waiting for Kong to be up..."
     sleep 5
 done
 
 # Add services and routes to Kong
-declare -A services_and_routes
-services_and_routes=(
-    ["product-service:8011"]="products"
-    ["product-consumer-service:8012"]="products"
-    ["order-service:8013"]="orders"
-    ["order-consumer-service:8014"]="orders"
-    ["inventory-service:8015"]="inventory"
-    ["inventory-consumer-service:8016"]="inventory"
-)
+services_and_routes="
+product-service:8011,products
+product-consumer-service:8012,products
+order-service:8013,orders
+order-consumer-service:8014,orders
+inventory-service:8015,inventory
+inventory-consumer-service:8016,inventory
+"
 
-for service in "${!services_and_routes[@]}"; do
-    route="${services_and_routes[$service]}"
+for entry in $services_and_routes; do
+    service=$(echo $entry | cut -d, -f1)
+    route=$(echo $entry | cut -d, -f2)
+    name=$(echo $service | cut -d: -f1)
+    port=$(echo $service | cut -d: -f2)
+    
     # Add service
     curl -i -X POST http://kong:8001/services/ \
-        --data "name=${service}" \
-        --data "url=http://${service}"
+        --data "name=${name}" \
+        --data "url=http://host.docker.internal:${port}"
 
     # Add route
-    curl -i -X POST http://kong:8001/services/${service}/routes \
+    curl -i -X POST http://kong:8001/services/${name}/routes \
         --data "paths[]=/api/${route}" \
         --data "strip_path=false"
 done
