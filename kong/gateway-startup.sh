14651ced7a0bc9673047d2db5c687beb322b0473
Ammar Aamir <ammaraamir609@gmail.com>
2024-08-03 23:39:21 +0500

    user service

--- kong/gateway-startup.sh
+++ kong/gateway-startup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "Starting gateway setup..."
-
 # Check if .env file exists and load environment variables
 if [ -f .env ]; then
     set -a
@@ -13,33 +11,74 @@ fi
 KONG_ADMIN_URL="http://localhost:8001"
 
 # Wait for Kong to be ready
-echo "Waiting for Kong to be ready..."
 until curl --output /dev/null --silent --head --fail $KONG_ADMIN_URL; do
     printf '.'
     sleep 5
 done
 
-echo "Kong is ready!"
+# Register services and add routes
 
-# Register services and add JWT plugin to each service
-echo "Registering product-service..."
-curl -i -v -X POST $KONG_ADMIN_URL/services/ \
+# Service: product-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
     --data "name=product-service" \
     --data "url=http://host.docker.internal:8011"
 
-echo "Adding route for product-service..."
-curl -i -v -X POST $KONG_ADMIN_URL/services/product-service/routes \
-    --data "paths[]=/products" \
+curl -i -X POST $KONG_ADMIN_URL/services/product-service/routes \
+    --data "paths[]=/product-service" \
     --data "strip_path=true"
 
-echo "Registering product-consumer-service..."
-curl -i -v -X POST $KONG_ADMIN_URL/services/ \
+# Service: product-consumer-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
     --data "name=product-consumer-service" \
     --data "url=http://host.docker.internal:8012"
 
-echo "Adding route for product-consumer-service..."
-curl -i -v -X POST $KONG_ADMIN_URL/services/product-consumer-service/routes \
-    --data "paths[]=/products" \
+curl -i -X POST $KONG_ADMIN_URL/services/product-consumer-service/routes \
+    --data "paths[]=/product-consumer-service" \
     --data "strip_path=true"
 
-echo "Gateway setup complete."
+# Service: order-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
+    --data "name=order-service" \
+    --data "url=http://host.docker.internal:8013"
+
+curl -i -X POST $KONG_ADMIN_URL/services/order-service/routes \
+    --data "paths[]=/order-service" \
+    --data "strip_path=true"
+
+# Service: order-consumer-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
+    --data "name=order-consumer-service" \
+    --data "url=http://host.docker.internal:8014"
+
+curl -i -X POST $KONG_ADMIN_URL/services/order-consumer-service/routes \
+    --data "paths[]=/order-consumer-service" \
+    --data "strip_path=true"
+
+# Service: inventory-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
+    --data "name=inventory-service" \
+    --data "url=http://host.docker.internal:8015"
+
+curl -i -X POST $KONG_ADMIN_URL/services/inventory-service/routes \
+    --data "paths[]=/inventory-service" \
+    --data "strip_path=true"
+
+# Service: inventory-consumer-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
+    --data "name=inventory-consumer-service" \
+    --data "url=http://host.docker.internal:8016"
+
+curl -i -X POST $KONG_ADMIN_URL/services/inventory-consumer-service/routes \
+    --data "paths[]=/inventory-consumer-service" \
+    --data "strip_path=true"
+
+# Service: user-service
+curl -i -X POST $KONG_ADMIN_URL/services/ \
+    --data "name=user-service" \
+    --data "url=http://host.docker.internal:8017"
+
+curl -i -X POST $KONG_ADMIN_URL/services/user-service/routes \
+    --data "paths[]=/user-service" \
+    --data "strip_path=true"
+
+echo "All services and routes have been registered successfully."
